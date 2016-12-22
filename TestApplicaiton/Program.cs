9ee310eb6cdcc226bf117914d5c3d455de300272
Narendra Ping <narendraping@hotmail.com>
2016-12-22 16:24:14 +0530

    Fabonacci series coded

--- TestApplicaiton/Program.cs
+++ TestApplicaiton/Program.cs
@@ -11,7 +11,30 @@ namespace TestApplicaiton
         static void Main(string[] args)
         {
             Console.WriteLine("Hello! I am learning how to use git remotely!");
-            Console.WriteLine("Hello again!");
+            Console.WriteLine("Hello again! Please Enter a number for fabo series : ");
+            int n = int.Parse( Console.ReadLine());
+            string series = FabonacciSeries(n);
+            Console.WriteLine(series);
+            Console.ReadKey();
+        }
+        static string FabonacciSeries(int n)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int a = 1;
+            int b = 1;
+            int s = 0;
+            sb.Append(a.ToString() + "," + b.ToString() + ",");
+            for(int i=3;i<= n;i++)
+            {
+                s = a + b;
+                a = b;
+                b = s;
+                sb.Append(s + ",");
+            }
+
+            return sb.ToString();
         }
     }
+    
 }
